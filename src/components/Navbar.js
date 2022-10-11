8428cfc1bdbc7f4d0d7f106e3f045ed0b71f7841
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-11 17:06:55 +0530

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import logo from "../../public/images/logo.png"
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <img src={logo} />
+        </div>
+    )
+}
